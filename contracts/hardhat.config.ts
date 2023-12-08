a9802902deceee5eb2b234bc2288a1002ff77713
prtk418 <prtkagarwal15@gmail.com>
2023-12-08 19:03:58 +0530

    hardhat project init

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+      },
+      {
+        version: "0.8.20",
+        settings: {},
+      },
+    ],
+  }
+};
+
+export default config;
