42d89f3fe18761f3bbd1f177863a131a61f04e75
prtk418 <prtkagarwal15@gmail.com>
2023-12-09 22:19:09 +0530

    onchain anon aadhaar verification

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,22 +1,33 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
+  const mandToken = await ethers.deployContract("MandeNetwork", ["0xD3db9D11c09cECd2E91bdE73F710dE6094179FA0"]);
+  await mandToken.waitForDeployment();
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Mand token deployed to ${mandToken.target}`
   );
+
+  // Deploying anon-aadhaar verification contracts
+  const verifier = await ethers.deployContract("Verifier");
+  await verifier.waitForDeployment();
+  const _verifierAddress = verifier.getAddress();
+  const appId = BigInt(
+    "1054725213937889195122827708128950524547440836608"
+  ).toString();
+  const anonAadhaarVerifier = await ethers.deployContract(
+    "AnonAadhaarVerifier",
+    [_verifierAddress, appId]
+  );
+  await anonAadhaarVerifier.waitForDeployment();
+  const _anonAadhaarVerifierAddress = anonAadhaarVerifier.getAddress();
+  
+  // Deploying trust drops
+  const trustDrops = await ethers.deployContract("TrustDrops", [mandToken.target, _anonAadhaarVerifierAddress]);
+  await trustDrops.waitForDeployment();
+  console.log(
+    `Trust drops deployed to ${trustDrops.target}`
+  );
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
