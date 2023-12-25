1c06fbade5ce1e208864069e9a7c9a1f8316ac71
Sweta Shaw <swt.shw94@gmail.com>
2023-12-25 18:47:53 +0530

    Initialize subgraph

---
+++ trustdrops-subgraph/tests/trust-drops-utils.ts
@@ -0,0 +1,97 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  OwnershipTransferred,
+  Staked,
+  TokensClaimed,
+  Unstaked
+} from "../generated/TrustDrops/TrustDrops"
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createStakedEvent(
+  staker: Address,
+  candidate: Address,
+  amount: BigInt,
+  cred: BigInt
+): Staked {
+  let stakedEvent = changetype<Staked>(newMockEvent())
+
+  stakedEvent.parameters = new Array()
+
+  stakedEvent.parameters.push(
+    new ethereum.EventParam("staker", ethereum.Value.fromAddress(staker))
+  )
+  stakedEvent.parameters.push(
+    new ethereum.EventParam("candidate", ethereum.Value.fromAddress(candidate))
+  )
+  stakedEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+  stakedEvent.parameters.push(
+    new ethereum.EventParam("cred", ethereum.Value.fromUnsignedBigInt(cred))
+  )
+
+  return stakedEvent
+}
+
+export function createTokensClaimedEvent(
+  candidate: Address,
+  amount: BigInt
+): TokensClaimed {
+  let tokensClaimedEvent = changetype<TokensClaimed>(newMockEvent())
+
+  tokensClaimedEvent.parameters = new Array()
+
+  tokensClaimedEvent.parameters.push(
+    new ethereum.EventParam("candidate", ethereum.Value.fromAddress(candidate))
+  )
+  tokensClaimedEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return tokensClaimedEvent
+}
+
+export function createUnstakedEvent(
+  staker: Address,
+  candidate: Address,
+  amount: BigInt
+): Unstaked {
+  let unstakedEvent = changetype<Unstaked>(newMockEvent())
+
+  unstakedEvent.parameters = new Array()
+
+  unstakedEvent.parameters.push(
+    new ethereum.EventParam("staker", ethereum.Value.fromAddress(staker))
+  )
+  unstakedEvent.parameters.push(
+    new ethereum.EventParam("candidate", ethereum.Value.fromAddress(candidate))
+  )
+  unstakedEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return unstakedEvent
+}
