d82deb42a2ed3cec456caccaef755e5af7e99b6e
prtk418 <prtkagarwal15@gmail.com>
2024-04-27 21:04:13 +0530

    wallet compatibility maybe fix

--- trust-drops-ui/src/context/DataContext.js
+++ trust-drops-ui/src/context/DataContext.js
@@ -24,31 +24,35 @@ const DataProvider = ({ children }) => {
   }, [accountAddress]);
 
   async function connectWallet() {
-    if (window.ethereum) {
-      if(window.ethereum._state.isUnlocked) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const { chainId } = await provider.getNetwork();
-        if (chainId != config.networks.testnet.chainId) {
-          await window.ethereum.request({
-            "method": "wallet_addEthereumChain",
-            "params": [
-              config.networks.testnet
-            ]
-          });
-        } else {
-          console.log("already connected");
-        }
+    try {
+      if (window.ethereum) {
+        // if(window.ethereum._state.isUnlocked) {
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const { chainId } = await provider.getNetwork();
+          if (chainId != config.networks.testnet.chainId) {
+            await window.ethereum.request({
+              "method": "wallet_addEthereumChain",
+              "params": [
+                config.networks.testnet
+              ]
+            });
+          } else {
+            console.log("already connected");
+          }
 
-        await provider.send("eth_requestAccounts", []);
-        const signer = provider.getSigner();
-        setSigner(signer);
-        setProvider(provider);
-        setAccountAddress(await signer.getAddress());
+          await provider.send("eth_requestAccounts", []);
+          const signer = provider.getSigner();
+          setSigner(signer);
+          setProvider(provider);
+          setAccountAddress(await signer.getAddress());
+        // } else {
+        //   console.log(`Please unlock ${window.ethereum.isMetaMask ? "metamask" : "wallet"}`)
+        // }
       } else {
-        console.log(`Please unlock ${window.ethereum.isMetaMask ? "metamask" : "wallet"}`)
+        console.log('EVM wallet not found')
       }
-    } else {
-      console.log('EVM wallet not found')
+    } catch (err) {
+      console.log("wallet connect failure", err);
     }
   }
 
