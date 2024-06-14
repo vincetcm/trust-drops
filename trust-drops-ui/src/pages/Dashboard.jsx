6387bd8f6b2b27f44251fb83af82b773772c6fad
prtk418 <prtkagarwal15@gmail.com>
2024-06-14 19:46:17 +0530

    updated reward estimated day

--- trust-drops-ui/src/pages/Dashboard.jsx
+++ trust-drops-ui/src/pages/Dashboard.jsx
@@ -497,7 +497,7 @@ function Dashboard() {
                       <div className='info-container  bg-[#7071E8] text-black flex items-center gap-2 w-full px-2'>
                         <img src={infoIcon}></img>
                         <div className='text-[10px] font-semibold text-center '>
-                          Rewards are distributed weekly 2PM UTC
+                          Next rewards will be distributed on 19th June 7AM UTC
                         </div>
                       </div>
                     </div>{' '}
